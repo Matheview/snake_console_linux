3b6f2e84f22c17b242881ce48d34f010ff83136e
Mateusz Baczek <matbak7@gmail.com>
2019-12-11 19:27:14 +0100

    initial structure script

--- snake_script.c
+++ snake_script.c
@@ -1,25 +1,67 @@
+#include <sys/ioctl.h>
 #include <stdio.h>
+#include <unistd.h>
 
+struct winsize w;
 char body_sign = "o"
 char food_sign = "@"
 char board_sign = "X"
 int body_length = 10
 
-# nie zmieniac
-IFS = ''
-int board_height = $(($(tput lines) - 12));
-int board_width = $(($(tput cols) - 2));
-int head_row_pos = $((board_height / 2));
-int head_col_pos = $((board_width / 2));
-int tail_row_pos = $((head_row_pos));
-int tail_col_pos = $((head_col_pos + $((body_length - 1))));
+ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+int board_height = w.ws_row - 12;
+int board_width = w.ws_col - 2;
+int head_row_pos = board_height/2;
+int head_col_pos = board_width/2;
+int tail_row_pos = head_row_pos;
+int tail_col_pos = head_col_pos + body_length - 1;
 int move_dir = 3;
 int move_av = -1;
 int points = body_length;
-char[body_length] body = ''
-declare move_by_row = ([0] = -1[1] = 0[2] = 1[3] = 0)
-declare move_by_col = ([0] = 0[1] = 1[2] = 0[3] = -1)
+char[body_length] body = '';
+int[] move_by_row = [-1, 0, 1, 0];
+int[] move_by_col = [0, 1, 0, -1]
 
+
+void draw_board() {
+
+}
+
+void draw_caterpilar() {
+
+}
+
+void throw_food() {
+
+}
+
+void init_game() {
+
+}
+
+int is_dead(int pos_row, int pos_col) {
+
+}
+
+void get_food() {
+
+}
+
+void move_body() {
+
+}
+
+int move_caterpillar() {
+
+}
+
+void get_key() {
+
+}
+
+void game_loop() {
+
+}
 
 void main() {
 
